6ec0f0dbb1e42530c68762d8485197fcdf9add20
Dimitar Petrov <buldozer9993@gmail.com>
2019-06-29 09:20:19 +0300

    Introduce environment configuration (#2)
    * Introduce environment configuration
    
    * Add tests

---
+++ config/config_test.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestConfig(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Config Suite")
+}
+
+var _ = Describe("config", func() {
+
+	Describe("Validate", func() {
+		var settings *Settings
+
+		BeforeEach(func() {
+			settings = DefaultSettings()
+		})
+
+		Context("when config is valid", func() {
+			It("returns no error", func() {
+				err := settings.Validate()
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+		})
+
+		Context("when port is missing", func() {
+			It("returns an error", func() {
+				settings.Server.Port = 0
+				err := settings.Validate()
+				Expect(err).Should(HaveOccurred())
+			})
+		})
+
+	})
+
+})
+
+
