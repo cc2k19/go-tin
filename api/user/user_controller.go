07ec04e607936fc12b229a11ad5495dfcd63f1f1
Dimitar Petrov <buldozer9993@gmail.com>
2019-06-29 21:03:16 +0300

    Add user endpoint implementation

--- api/user/user_controller.go
+++ api/user/user_controller.go
@@ -1,7 +1,10 @@
 package user
 
 import (
+	"github.com/cc2k19/go-tin/api"
 	"github.com/cc2k19/go-tin/storage"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -10,7 +13,27 @@ type controller struct {
 }
 
 func (c *controller) add(wr http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 
+	bodyReader, err := r.GetBody()
+	if err != nil {
+		log.Printf("Could not extract body: %s\n", err)
+		api.WriteResponse(wr, http.StatusBadRequest, api.ErrorResponse{Error: err.Error()})
+	}
+	defer bodyReader.Close()
+
+	body, err := ioutil.ReadAll(bodyReader)
+
+	if err != nil {
+		log.Printf("Could not extract body: %s\n", err)
+		api.WriteResponse(wr, http.StatusBadRequest, api.ErrorResponse{Error: err.Error()})
+	}
+
+	users, err := c.repository.AddUser(ctx, body)
+	if err != nil {
+		log.Printf("Persisting user failed: %s\n", err)
+	}
+	api.WriteResponse(wr, http.StatusBadRequest, users)
 }
 
 func (c *controller) getByUsername(wr http.ResponseWriter, r *http.Request) {
